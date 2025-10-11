ef7a65bc47597cc95db10549a91b6c8b395f3549
evolutionimpactinitiative <evolutionimpactinitiative@gmail.com>
2025-10-11 10:35:36 +0100

    changes added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,30 @@ const manrope = Manrope({
 export const metadata: Metadata = {
   title: "Evolution Impact Initiative",
   description: "Empowering Communities Inspiring Change",
+  openGraph: {
+    title: "Evolution Impact Initiative",
+    description: "Empowering Communities Inspiring Change",
+    url: "https://www.evolutionimpactinitiative.co.uk",
+    siteName: "Evolution Impact Initiative",
+    images: [
+      {
+        url: "https://www.evolutionimpactinitiative.co.uk/assets/thumbnail.png",
+        width: 1200,
+        height: 630,
+        alt: "Evolution Impact Initiative",
+      },
+    ],
+    locale: "en_GB",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Evolution Impact Initiative",
+    description: "Empowering Communities Inspiring Change",
+    images: [
+      "https://www.evolutionimpactinitiative.co.uk/assets/thumbnail.jpg",
+    ],
+  },
 };
 
 export default function RootLayout({
